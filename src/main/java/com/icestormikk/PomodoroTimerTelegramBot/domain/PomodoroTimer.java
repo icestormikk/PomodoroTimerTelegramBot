400d69b5cca120ae5dc416651a02b47427ce2da6
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-02 23:39:36 +0300

    Добавил аннотацию для создания конструктора со всеми аргументами

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/domain/PomodoroTimer.java
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/domain/PomodoroTimer.java
@@ -1,10 +1,12 @@
 package com.icestormikk.PomodoroTimerTelegramBot.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class PomodoroTimer {
     @Id
     public String id = "";
