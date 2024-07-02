8268afb423221753483f924173d93f1a32eb3aa0
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-02 23:08:48 +0300

    Initial commit

---
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/PomodoroTimerTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.icestormikk.PomodoroTimerTelegramBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroTimerTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroTimerTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icestormikk/PomodoroTimerTelegramBot/PomodoroTimerTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icestormikk.PomodoroTimerTelegramBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomodoroTimerTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
