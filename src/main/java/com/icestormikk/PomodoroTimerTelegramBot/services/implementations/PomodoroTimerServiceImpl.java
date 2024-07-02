a9d28a5c0de866b92af5575fd34d6044faa81b25
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-02 23:40:16 +0300

    Обновил описание одного из выводимых логов, а также доработал методы для обновления и удаления объектов

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/services/implementations/PomodoroTimerServiceImpl.java
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/services/implementations/PomodoroTimerServiceImpl.java
@@ -33,7 +33,7 @@ public class PomodoroTimerServiceImpl implements PomodoroTimerService {
         Optional<PomodoroTimer> timer = pomodoroTimerRepository.findById(id);
 
         if (timer.isEmpty()) {
-            log.warn("Объект класса PomodoroTimer с id {} был не найден", id);
+            log.warn("Во время поиска объект класса PomodoroTimer с id {} был не найден", id);
             throw new PomodoroTimerNotFound();
         }
 
@@ -59,11 +59,37 @@ public class PomodoroTimerServiceImpl implements PomodoroTimerService {
     @Override
     public PomodoroTimer updateTimerById(String id, PomodoroTimerDto pomodoroTimerDto) {
         log.info("Получен запрос на обновление объекта класса PomodoroTimer");
-        throw new UnsupportedOperationException();
+
+        Optional<PomodoroTimer> timer = pomodoroTimerRepository.findById(id);
+        if (timer.isEmpty()) {
+            log.warn("Во время обновления объект класса PomodoroTimer с id {} был не найден", id);
+            throw new PomodoroTimerNotFound();
+        }
+
+        PomodoroTimer updatedTimer = new PomodoroTimer(id, pomodoroTimerDto.label);
+        try {
+            PomodoroTimer result = pomodoroTimerRepository.save(updatedTimer);
+            log.info("Обновление объекта класса PomodoroTimer c id {} прошло успешно", id);
+            return result;
+        } catch (Exception e) {
+            log.error("Во время обновления объекта класса PomodoroTimer произошла ошибка: {}", e.getMessage());
+            throw e;
+        }
     }
 
     @Override
     public void deleteTimerById(String id) {
-        throw new UnsupportedOperationException();
+        Optional<PomodoroTimer> timer = pomodoroTimerRepository.findById(id);
+        if (timer.isEmpty()) {
+            log.warn("Во время удаления объект класса PomodoroTimer с id {} был не найден", id);
+            throw new PomodoroTimerNotFound();
+        }
+
+        try {
+            pomodoroTimerRepository.deleteById(id);
+        } catch (Exception e) {
+            log.error("Во время удаления объекта класса PomodoroTimer произошла ошибка: {}", e.getMessage());
+            throw e;
+        }
     }
 }
