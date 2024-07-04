26dc632929a2e93126d7ce65d470a59e050e03ef
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-04 14:04:21 +0300

    Добавил новые команды боту

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/telegram/PomodoroTelegramBotCommands.java
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/telegram/PomodoroTelegramBotCommands.java
@@ -2,4 +2,8 @@ package com.icestormikk.PomodoroTimerTelegramBot.telegram;
 
 public class PomodoroTelegramBotCommands {
     public static final String START = "/start";
+    public static final String TIMER_CREATE = "/timer";
+    public static final String TIMER_START = "/timerstart";
+    public static final String TIMER_STOP = "/timerstop";
+    public static final String STATS = "/stats";
 }
