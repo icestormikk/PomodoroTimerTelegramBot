524a816992607d17989b1bc0b9fc2bd94ac6c455
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-11 23:44:34 +0300

    Обновил реализация таймера, добавил новые поля и метод run

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/domain/PomodoroTimer.java
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/domain/PomodoroTimer.java
@@ -1,15 +1,38 @@
 package com.icestormikk.PomodoroTimerTelegramBot.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import com.icestormikk.PomodoroTimerTelegramBot.domain.interfaces.TimerListeners;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.annotation.Id;
 
-@RequiredArgsConstructor
-@AllArgsConstructor
-public class PomodoroTimer {
+@Slf4j
+public class PomodoroTimer extends Thread {
     @Id
-    public String id = "";
-    @NonNull
+    public String id;
     public String label;
+    public Long timeInMs;
+    @Setter
+    private TimerListeners listeners;
+
+    public PomodoroTimer(String label, Long timeInMs) {
+        this.timeInMs = timeInMs;
+        this.label = label;
+    }
+
+    public PomodoroTimer(String id, String label, Long timeInMs) {
+        this.id = id;
+        this.label = label;
+        this.timeInMs = timeInMs;
+    }
+
+    @Override
+    public void run() {
+        listeners.onTimerStart(this);
+        try {
+            Thread.sleep(timeInMs);
+        } catch (InterruptedException e) {
+            log.error("An error occurred while the timer was running with id {}: {}", this.id, e.getMessage());
+        }
+        listeners.onTimerStop(this);
+    }
 }
