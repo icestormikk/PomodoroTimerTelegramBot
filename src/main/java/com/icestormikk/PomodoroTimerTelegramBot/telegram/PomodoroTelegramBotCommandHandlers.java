a41082fac867a4a5961be4ada118f166bd241363
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-12 01:27:49 +0300

    Связал ввод пользователя и обработчики для двух новых команд

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/telegram/PomodoroTelegramBotCommandHandlers.java
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/telegram/PomodoroTelegramBotCommandHandlers.java
@@ -3,6 +3,8 @@ package com.icestormikk.PomodoroTimerTelegramBot.telegram;
 import com.icestormikk.PomodoroTimerTelegramBot.telegram.exceptions.CommandHandlerNotFound;
 import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.CreateTimerCommandHandler;
 import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.StartCommandHandler;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.StartTimerCommandHandler;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.StopTimerCommandHandler;
 import com.icestormikk.PomodoroTimerTelegramBot.telegram.interfaces.CommandHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -14,12 +16,14 @@ import java.util.Map;
 public class PomodoroTelegramBotCommandHandlers {
     private static final Map<String, CommandHandler> commandsMap = Map.of(
         PomodoroTelegramBotCommands.START, new StartCommandHandler(),
-        PomodoroTelegramBotCommands.TIMER_CREATE, new CreateTimerCommandHandler()
+        PomodoroTelegramBotCommands.TIMER_CREATE, new CreateTimerCommandHandler(),
+        PomodoroTelegramBotCommands.TIMER_START, new StartTimerCommandHandler(),
+        PomodoroTelegramBotCommands.TIMER_STOP, new StopTimerCommandHandler()
     );
 
     public static BotApiMethod<?> handleCommand(Update update) throws CommandHandlerNotFound {
         String userInput = update.getMessage().getText();
-        CommandHandler handler = commandsMap.get(userInput);
+        CommandHandler handler = commandsMap.get(userInput.split("\\s")[0]);
 
         if (handler == null) {
             log.error("Couldn't find a handler for the command {}", userInput);
