e2a67ecc3811e285036327da8d8f5a1b8e12f6a1
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-12 23:55:06 +0300

    Удалил реализации для репозитория и сервиса для работы с объектами PomodoroTimer

--- src/main/java/com/icestormikk/PomodoroTimerTelegramBot/repositories/PomodoroTimerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.icestormikk.PomodoroTimerTelegramBot.repositories;
-
-import com.icestormikk.PomodoroTimerTelegramBot.domain.PomodoroTimer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PomodoroTimerRepository extends MongoRepository<PomodoroTimer, String> {}
