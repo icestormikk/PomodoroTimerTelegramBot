6e75de6fddf8addb4bacba33ba4d6008a52862cb
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-12 23:19:32 +0300

    Первая реализация инструмента для обработки Callback'ов

---
+++ src/main/java/com/icestormikk/PomodoroTimerTelegramBot/telegram/PomodoroTelegramBotCallbackHandlers.java
@@ -0,0 +1,39 @@
+package com.icestormikk.PomodoroTimerTelegramBot.telegram;
+
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.exceptions.CallbackHandlerNotFound;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.exceptions.InvalidUserSessionException;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.callbacks.PomodoroTimerDescriptionCallbackHandler;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.handlers.callbacks.PomodoroTimerLabelCallbackHandler;
+import com.icestormikk.PomodoroTimerTelegramBot.telegram.interfaces.CallbackHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.Map;
+
+@Slf4j
+public class PomodoroTelegramBotCallbackHandlers {
+    private static final Map<PomodoroTimerState, CallbackHandler> handlers = Map.of(
+        PomodoroTimerState.FETCHING_TASK_LABEL, new PomodoroTimerLabelCallbackHandler(),
+        PomodoroTimerState.FETCHING_TASK_DESCRIPTION, new PomodoroTimerDescriptionCallbackHandler()
+    );
+
+    public static BotApiMethod<?> handleCallback(Update update, PomodoroTimerUserSession session) throws CallbackHandlerNotFound {
+        PomodoroTimerState state = session.state;
+        CallbackHandler handler = handlers.get(state);
+
+        if (handler == null) {
+            log.error("Couldn't find a handler for the state {}", state.name());
+            throw new CallbackHandlerNotFound();
+        }
+
+        try {
+            BotApiMethod<?> response = handler.handle(update);
+            log.info("The message was successfully processed");
+            return response;
+        } catch (InvalidUserSessionException e) {
+            log.error("The message could not be processed. An error occurred during processing.");
+            return null;
+        }
+    }
+}
